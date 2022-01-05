914921bd9d469a449e9dad93ff1c25dfb75d7258
aLeX-c-m <alexacatherine.marshall@gmail.com>
2022-01-05 11:51:02 -0500

    reverted pre menu added html top margin

--- page.php
+++ page.php
@@ -1,18 +1,24 @@
 <?php
     get_header(); 
 ?>  
+<div class="content-page-container" id="c1">
+	<div class="content-page-block">
+		<div class="content-page-block-text">
+            <?php 
+                if( have_posts() ){
+                    while( have_posts() ){
+                    
+                        the_post();
+                    
+                        the_content(); 
 
-<?php 
-    if( have_posts() ){
-        while( have_posts() ){
+                    }
+                }
+            ?>
+        </div>
+    </div>
+</div>
 
-            the_post();
-
-            the_content(); 
-            
-        }
-    }
-?>
 <?php
     get_footer(); 
 ?>  
